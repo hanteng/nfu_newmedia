b963a1653ce017ff0295d4f2b04d95666da5228f
Han-Teng Liao <hanteng@gmail.com>
2017-05-17 17:34:19 +0800

    1st batch with README.md
    首次上傳

---
+++ README.md
@@ -0,0 +1,19 @@
+# @中山大学南方学院 2016 - 2017 文学与传媒系【Python】
+
+# 簡介 
+
+## webapp_zh
+
+### vsearch.py
+
+找介詞
+
+### vsearch4web.py
+
+找介詞app
+
+Note: 來源
+
+## webapp_zh\templates
+
+
---
+++ webapp_zh/vsearch4web.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, escape
+from vsearch import search4letters
+
+app = Flask(__name__)
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    """Log details of the web request and the results."""
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+
+
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    """Extract the posted data; perform the search; return results."""
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = '以下是您的结果：'
+    results = str(search4letters(phrase, letters))
+    log_request(request, results)
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results,)
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    """Display this webapp's HTML form."""
+    return render_template('entry.html',
+                           the_title='欢迎来到网上 汉语找介词！')
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    """Display the contents of the log file as a HTML table."""
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('表单内容', '访问者IP', '浏览器', '运行结果')
+    return render_template('viewlog.html',
+                           the_title='查看日志',
+                           the_row_titles=titles,
+                           the_data=contents,)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
