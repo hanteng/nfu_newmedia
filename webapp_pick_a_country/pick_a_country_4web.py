0e2ac46f5e80a2a0b0d581062507eb857b756ba6
Han-Teng Liao <hanteng@gmail.com>
2017-07-20 11:13:22 +0800

    使用高階HTML datalist
    使用高階HTML datalist

--- webapp_pick_a_country/pick_a_country_4web.py
+++ webapp_pick_a_country/pick_a_country_4web.py
@@ -4,7 +4,7 @@ import country
 c = country.country_list_name()
 c_list = [c.data[k] for k in sorted(c.data.keys())]
 c_dict_reverse = {v:k for k, v in c.data.items()}
-
+c_list_wcode = [(k, c.data[k]) for k in sorted(c.data.keys())] # 含代码
 
 from flask import Flask, render_template, request, escape
 
@@ -16,14 +16,17 @@ app = Flask(__name__)
 def entry_page() -> 'html':
     """Display this webapp's HTML form."""
     return render_template('entry.html',
-                           the_list_items = c_list ,
+                           the_list_items = c_list_wcode ,
                            the_title='欢迎来到网上选国！')
 
 @app.route('/pick_a_country', methods=['POST'])
 def pick_a_color() -> 'html':
     """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
-    user_country_name = request.form['user_country']	
-    user_country_code = c_dict_reverse[user_country_name]	
+    try:
+        user_country_code = request.form['user_country']	
+        user_country_name = c.data[user_country_code]	
+    except: #用户输入可能有问题
+        user_country_name = '您输入的国家代码在数据库找不到'
     return render_template('results.html',
                            the_title = '以下是您选取的国：',
                            the_country_code = user_country_code,
